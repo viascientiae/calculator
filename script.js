46e1d03efb2c9c203d268b55e1c003041043f351
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2023-09-18 23:14:35 +0530

    Create operate() to call functions for basic math operations

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ function divide(num1, num2) {
 }
 
 let number1, number2, operator;
+
+function operate(operator, number1, number2) {
+    
+    switch(operator) {
+        case "+" :
+        return add(number1, number2);
+        break;
+        case "-" :
+        return subtract(number1, number2);
+        break;
+        case "*" :
+        return multiply(number1, number2);
+        break;
+        case "/" :
+        return divide(number1, number2);
+        break;
+        default:
+        return "ERROR";
+        break;
+    }
+}
